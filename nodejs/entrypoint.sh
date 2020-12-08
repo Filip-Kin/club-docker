ed751b826e1f9c72fa06186ace2766859051e554
Filip Kin <filip@kinmails.com>
2020-12-08 11:35:00 -0500

    magic

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -2,11 +2,18 @@
 cd /home/container
 
 # Output Current Node Version
+echo "Node.JS Version: "
 node --version
+echo "NPM Version: "
+npm --version
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ""
+echo "container@c:/home/container$ npm install"
+npm install
+
+echo "Starting Node"
 
 # Run the Server
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "container@c:/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
