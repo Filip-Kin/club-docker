29e0819480c78e4fd985af89b66631889ff7f444
Filip Kin <filip@kinmails.com>
2021-03-15 12:48:59 -0400

    node 14 and go 16

---
+++ club-go.1.16/entrypoint.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+cd /home/container
+
+# Output Current Go Version
+go version
+
+echo ""
+
+if [ ! -e "app" ]; then
+  mkdir app
+fi
+
+cd app
+
+GIT_CHANGED=0
+
+# environment variables for everything, copied muiltiple times
+# just in case
+export PORT=${SERVER_PORT}
+export HTTP_PORT=${SERVER_PORT}
+
+export CLUB_HOST=172.18.0.1
+export RETHINKDB_HOST=${CLUB_HOST}
+export MYSQL_HOST=${CLUB_HOST}
+export REDIS_HOST=${CLUB_HOST}
+export GOPATH=/home/container/go
+
+if [ -n "${GIT_REPO}" ]; then
+  echo "Git Repository: ${GIT_REPO}"
+  if [ -e ".git" ]; then
+    git remote set-url "$(git remote)" "${GIT_REPO}"
+    git reset --hard
+    git checkout "${GIT_BRANCH}"
+    git pull origin "${GIT_BRANCH}" --recurse-submodules | grep -q -v 'Already up-to-date.' && GIT_CHANGED=1
+  else
+    git init .
+    git remote add origin "${GIT_REPO}"
+    git pull origin "${GIT_BRANCH}" --recurse-submodules -q
+  fi
+fi
+
+echo $ go mod download
+go mod download
+
+if [ -n "${GIT_REPO}" ] && [ -n "${BUILD_SCRIPT}" ]; then
+  if [ ! -e "/home/container/.build.config" ]; then
+    GIT_CHANGED=1
+  else
+    if ! [ "${GIT_REPO};${GIT_BRANCH};${BUILD_SCRIPT}" = "$(cat /home/container/.build.config)" ]; then
+      GIT_CHANGED=1
+    fi
+  fi
+  echo "${GIT_REPO};${GIT_BRANCH};${BUILD_SCRIPT}" > /home/container/.build.config
+  if [ "$GIT_CHANGED" = "1" ]; then
+    BUILD_SCRIPT_MODIFIED=`eval echo $(echo ${BUILD_SCRIPT} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+    echo $ "${BUILD_SCRIPT_MODIFIED}"
+    ${BUILD_SCRIPT_MODIFIED}
+  fi
+fi
+
+START_SCRIPT_MODIFIED=`eval echo $(echo ${START_SCRIPT} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
+if [ -n "${DOMAIN}" ]; then
+  echo Notifying Club Sever of the domain configuration.
+  wget "http://${CLUB_HOST}:1000/proxy?domain=${DOMAIN}&hostname=${HOSTNAME}" -O -
+fi
+
+echo "Starting Server"
+
+export NODE_ENV=production
+
+echo $ "${START_SCRIPT_MODIFIED}"
+${START_SCRIPT_MODIFIED}
--- update-docker.sh
+++ update-docker.sh
@@ -18,6 +18,8 @@ docker_deploy() {
 
 docker_deploy club-base
 docker_deploy club-nodejs 12
+docker_deploy club-nodejs 14
 docker_deploy club-nodejs 15
 docker_deploy club-go 1.15
+docker_deploy club-go 1.16
 docker_deploy club-web basic
